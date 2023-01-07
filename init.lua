2e6b200033a1f7435e55a3a4a920a0fec537a78e
Tobias Newmiwaka <newmi1988@gmail.com>
2023-01-07 12:32:20 +0100

    added shortcut for fuzzy buffer search

--- init.lua
+++ init.lua
@@ -70,6 +70,7 @@ vim.api.nvim_set_keymap('n','<C-B>',':NERDTreeToggle<CR>', { noremap = true, sil
 
 ---- fzf file search
 vim.api.nvim_set_keymap('n','<C-p>',':GFiles<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n','<leader>b',':Buffers<CR>', { noremap = true, silent = true})
 
 ---- fzf with ripgrep (see README)
 vim.api.nvim_set_keymap('n','<C-g>',':Rg<CR>',{ noremap = true, silent = true})
