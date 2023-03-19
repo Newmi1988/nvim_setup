5a4e2e6f7dd63cfb9978729ced6318abfb0a372e
Tobias Newmiwaka <newmi1988@gmail.com>
2023-03-19 22:09:01 +0100

    feat(python-dap): python setup
    TODO: if there is no .venv fallback to global python

--- after/plugin/dapui.lua
+++ after/plugin/dapui.lua
@@ -1,12 +1,13 @@
 local dapui = require("dapui")
+dapui.setup()
 
--- vim.keymap.set('n', '<leader>do', dapui.toggle(), {
---   desc = 'Dapui: toggle ui',
--- })
+vim.keymap.set('n', '<leader>do', function() dapui.toggle() end, {
+  desc = 'Dapui: toggle ui',
+})
 
--- vim.keymap.set('v', '<leader>dv', dapui.eval(), {
---   desc = 'Dapui: toggle ui',
--- })
+vim.keymap.set('v', '<leader>dv', function() dapui.eval() end, {
+  desc = 'Dapui: toggle ui',
+})
 
 
 --- dap options
@@ -20,3 +21,38 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
+
+--- python config
+local dap = require('dap')
+dap.adapters.python = {
+  type = 'executable',
+  command = os.getenv('VIRTUAL_ENV') .. '/.venv/bin/python',
+  args = { '-m', 'debugpy.adapter' },
+}
+
+
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>tbp', function()
+  require(
+    'dap'
+  ).toggle_breakpoint()
+end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dpv', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
