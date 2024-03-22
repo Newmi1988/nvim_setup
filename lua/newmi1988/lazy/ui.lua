4b0ef4cd585da73692172c97ca3250e90976ecc0
Tobias Newmiwaka <newmi1988@gmail.com>
2024-03-22 21:54:16 +0100

    feat: add twilight plugin to nvim

--- lua/newmi1988/lazy/ui.lua
+++ lua/newmi1988/lazy/ui.lua
@@ -510,17 +510,25 @@ return {
     end
   },
   {
-  'stevearc/aerial.nvim',
-  config = function ()
-    require("aerial").setup({
-    -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-    on_attach = function(bufnr)
-      -- Jump forwards/backwards with '{' and '}'
-      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-    end,
-    })
-    vim.keymap.set("n", "<leader>so", "<cmd>AerialToggle!<CR>")
-  end
+    'stevearc/aerial.nvim',
+    config = function()
+      require("aerial").setup({
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+      })
+      vim.keymap.set("n", "<leader>so", "<cmd>AerialToggle!<CR>")
+    end
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   },
 }
