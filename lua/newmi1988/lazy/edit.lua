9a22920f41fe815b9d301307dfd2641d53cfe708
Tobias Newmiwaka <newmi1988@gmail.com>
2024-07-05 19:22:43 +0200

    refactor: remove autosession - interferes with workflow

--- lua/newmi1988/lazy/edit.lua
+++ lua/newmi1988/lazy/edit.lua
@@ -269,17 +269,4 @@ return {
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
   },
-  {
-    "rmagatti/auto-session", -- auto save session
-    config = function()
-      require("auto-session").setup({
-        log_level = "error",
-        auto_session_suppress_dirs = {
-          "~/", "~/Downloads", "~/Documents"
-        },
-        auto_session_use_git_branch = true,
-        auto_save_enabled = true
-      })
-    end
-  },
 }
