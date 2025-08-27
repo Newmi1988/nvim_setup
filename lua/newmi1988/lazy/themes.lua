ae255d711ce5bb86cdc27d73aa57ae5ecb1e714d
Tobias Newmiwaka <newmi1988@gmail.com>
2025-08-27 22:24:26 +0200

    faat: change color scheme to kanagawa and update plugins

--- lua/newmi1988/lazy/themes.lua
+++ lua/newmi1988/lazy/themes.lua
@@ -1,8 +1,6 @@
 return {
   ---- tokio night
   'folke/tokyonight.nvim',
-  ---- colorscheme inspired by kanagawa
-  "rebelot/kanagawa.nvim",
   -- A dark and light Neovim theme written in fennel
   'nyoom-engineering/oxocarbon.nvim',
   {
@@ -103,7 +101,39 @@ return {
         highlight_overrides = {},
       })
 
-      vim.cmd [[colorscheme catppuccin]]
+      -- vim.cmd [[colorscheme catppuccin]]
     end
-  }
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      -- Default options:
+      require('kanagawa').setup({
+        compile = false,             -- enable compiling the colorscheme
+        undercurl = true,            -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = { bold = true, italic = true},
+        keywordStyle = { bold = true},
+        statementStyle = { bold = true },
+        typeStyle = { italic = true},
+        transparent = false,         -- do not set background color
+        dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+        colors = {                   -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave",              -- Load "wave" theme
+        background = {               -- map the value of 'background' option to a theme
+          dark = "wave",           -- try "dragon" !
+          light = "lotus"
+        }
+      })
+      -- setup must be called before loading
+      require("kanagawa").load("wave")
+    end,
+  },
 }
