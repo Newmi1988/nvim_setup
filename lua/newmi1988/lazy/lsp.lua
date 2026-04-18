a033f036f9726dccb58f74991cfbe9d5708a2fe9
Tobias Newmiwaka <newmi1988@gmail.com>
2026-04-18 13:51:08 +0200

    chore: update plugins

--- lua/newmi1988/lazy/lsp.lua
+++ lua/newmi1988/lazy/lsp.lua
@@ -195,8 +195,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-      -- Treesitter Config
-      require 'nvim-treesitter.configs'.setup {
         ensure_installed = {
           "c",
           "lua",
@@ -210,17 +208,7 @@ return {
           "markdown_inline",
           "vimdoc"
         },                   -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-        ignore_install = {}, -- List of parsers to ignore installing
-        highlight = {
-          enable = true,     -- false will disable the whole extension
-          disable = {},      -- list of language that will be disabled
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-      }
+        require('nvim-treesitter').install(ensure_installed)
     end
   },
   'nvim-treesitter/nvim-treesitter-context',
