8c27509231e042bc089a5bc099cbb63ed5adfeda
Tobias Newmiwaka <newmi1988@gmail.com>
2026-04-22 22:52:13 +0200

    feat: add schema store configuration

--- init.lua
+++ init.lua
@@ -27,6 +27,7 @@ local language_servers = {
   'rust_analyzer',
   'ts_ls',
   'yamlls',
+  'jsonls',
 }
 
 require("mason-lspconfig").setup {
@@ -34,6 +35,33 @@ require("mason-lspconfig").setup {
 }
 
 vim.lsp.enable(language_servers)
+
+-- Schema completion
+--- configure the json schemastore
+vim.lsp.config("jsonls", {
+    settings = {
+        json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+        },
+    },
+})
+
+--- configure the yamlls schemastore
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = {
+      schemaStore = {
+        -- You must disable built-in schemaStore support if you want to use
+        -- this plugin and its advanced options like `ignore`.
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
+      },
+      schemas = require('schemastore').yaml.schemas(),
+    },
+  },
+})
 
 -- Terminal autocommands
 ---- Start terminal with insert mode
